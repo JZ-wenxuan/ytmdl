2e1ecfebab357dc6fc16acc9567e7269253e6baf
Wenxuan Zhu <jack.zwx@icloud.com>
2022-09-22 21:05:59 -0400

    init my own project

---
+++ src/ytmurl/get.py
@@ -0,0 +1,44 @@
+from ytmusicapi import YTMusic
+from yt_dlp import YoutubeDL
+import logging
+import json
+
+def get(query, duration=None, duration_tolerance=1000, logger=logging.getLogger()):
+    # sanitize query
+    query = query.replace('-', '')
+
+    # ytm search
+    ytmusic = YTMusic()
+    results = ytmusic.search(query.replace('-', '')) # example: '街角の风景 Falcom Sound Team J.D.K. 英雄伝説 零の軌跡 オリジナルサウンドトラック'
+    logger.debug(f'Got results from ytmusic: {json.dumps(results)}')
+
+    # select song
+    def select_song(results):
+        for r in results:
+            if all([
+                r['category'] in ['Top result', 'Songs', 'Videos'],
+                r['resultType'] in ('song', 'video'),
+                duration is None or ('duration_seconds' in r and abs(r['duration_seconds'] * 1000  - duration) <= duration_tolerance),
+            ]):
+                return r['videoId']
+        logger.exception(f'No match for "{query}"')
+        return None
+
+    songId = select_song(results)
+    logger.info(f'Retrieved Song Id: {songId}')
+
+    # get url with yt-dlp
+    ydl_opts = {
+        'format' : 'bestaudio',
+        'logger' : logger,
+    }
+    with YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(f'https://www.youtube.com/watch?v={songId}', download=False)
+        info = ydl.sanitize_info(info)
+
+    logger.debug(f'Got json: {json.dumps(info)}')
+
+    url = info['url']
+
+    logger.info(f'Got URL: {url}')
+    return url
--- tests/test_get.py
+++ tests/test_get.py
@@ -1,13 +1,12 @@
 import unittest
 
-from examplepy.module1 import Number
+from ytmurl.get import get
 
 
 class TestSimple(unittest.TestCase):
 
     def test_add(self):
-        self.assertEqual((Number(5) + Number(6)).value, 11)
-
+        self.assertIsNotNone(get('降水确率10% Falcom Sound Team J.D.K.'))
 
 if __name__ == '__main__':
     unittest.main()
